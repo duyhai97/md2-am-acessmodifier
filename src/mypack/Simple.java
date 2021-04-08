f29f8789256aabbbd05b41a56d5becf3349ceaf7
duyhai97 <79962575+duyhai97@users.noreply.github.com>
2021-04-08 16:34:44 +0700

    a

---
+++ src/mypack/Simple.java
@@ -0,0 +1,14 @@
+package mypack;
+import pack.*;
+class Simple  extends A {
+    public static void main(String[] args) {
+//        A b = new A();
+//        b.aaa = 40;
+//        System.out.println(b.aaa);
+    A b2 = new A();
+    b2.msg2();
+    b2.a2 = 005;
+        System.out.println(b2.a2);
+    }
+
+}
---
+++ src/pack/A.java
@@ -0,0 +1,19 @@
+package pack;
+public class A {
+//    private int a = 40;
+//    private void msg(){
+//        System.out.println("hello");
+//    }
+//    int a1 = 40;
+//    void msg1(){
+//        System.out.println("hello");
+//    }
+
+    private int a2 = 40;
+
+
+ private void msg2(){
+        System.out.println("heeelllo");
+    }
+
+}
